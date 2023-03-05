ada591b2b2a6a2ce83700f8f7ac86d5e6bc8776a
Abhijith Shaji <112998926+abhijeeeth@users.noreply.github.com>
2023-03-05 20:49:38 +0530

    Add files via upload

---
+++ packages/mypack/sample.java
@@ -0,0 +1,16 @@
+package mypack;
+import java.io.*;
+public class sample
+    {
+    int x,y;
+    public sample(int a,int b)
+        {
+            x=a;
+            y=b;
+        }
+            public void sum()
+            {
+                int c=x+y;
+                System.out.println("sum="+c);
+            }
+    }
---
+++ packages/pack.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import mypack.*;
+public class pack
+{
+public static void main(String args[])
+{
+sample s= new sample(4,6);
+s.sum();
+}
+}
